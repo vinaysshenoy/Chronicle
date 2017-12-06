7a7dde11690497879e16b0f12c725610340b12f5
Vinay Shenoy <vinaysshenoy@gmail.com>
2017-12-06 21:32:31 +0530

    Added method to Chronicle to watch and query

--- core/src/main/kotlin/com/vinaysshenoy/chronicle/Chronicle.kt
+++ core/src/main/kotlin/com/vinaysshenoy/chronicle/Chronicle.kt
@@ -1,9 +1,13 @@
 package com.vinaysshenoy.chronicle
 
-class Chronicle {
+class Chronicle(val store: Store) {
 
   @JvmOverloads
   fun did(event: String, eventTimeMillis: Long = System.currentTimeMillis()) {
 
   }
+
+  fun watch(watch: Watch) = Watcher.Builder(watch)
+
+  fun query(name: String = "") = Watch.Builder(store, name)
 }
