88bf5c93e370129ba83b569f54393d4950335ec9
Vinay Shenoy <vinaysshenoy@gmail.com>
2017-12-06 21:12:00 +0530

    Added WatchBuilder

--- core/src/main/kotlin/com/vinaysshenoy/chronicle/Watch.kt
+++ core/src/main/kotlin/com/vinaysshenoy/chronicle/Watch.kt
@@ -1,5 +1,33 @@
 package com.vinaysshenoy.chronicle
 
-class Watch @JvmOverloads constructor(val name: String = "") {
+import com.vinaysshenoy.chronicle.expr.Equal
+import com.vinaysshenoy.chronicle.expr.Expression
+import com.vinaysshenoy.chronicle.expr.GreaterThanOrEqualTo
+import com.vinaysshenoy.chronicle.expr.LesserThanOrEqualTo
 
+class Watch private constructor(val name: String = "") {
+
+
+  class Builder(private val name: String = "") {
+
+    fun timesDone(event: String, count: Long) = timesDone(event, Equal(count))
+
+    fun timesDone(event: String, expr: Expression): Builder {
+      return this
+    }
+
+    fun hasDone(event: String) = timesDone(event, GreaterThanOrEqualTo(1))
+
+    fun timesDoneSince(event: String, timeMillis: Long, count: Long) = timesDoneSince(event, timeMillis, Equal(count))
+
+    fun timesDoneSince(event: String, timeMillis: Long, expr: Expression): Builder {
+      return this
+    }
+
+    fun timeSinceLastOccurrence(event: String, durationMillis: Long) = timeSinceLastOccurrence(event, LesserThanOrEqualTo(durationMillis))
+
+    fun timeSinceLastOccurrence(event: String, expr: Expression): Builder {
+      return this
+    }
+  }
 }
